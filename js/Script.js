88cd0652e072bb121a8c087835f6d1959c3b8cf5
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-05-18 21:32:22 +0530

    link nav tags

---
+++ js/Script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing',{
+    strings:["","Web Developer","Fullstack Developer","Moblie App Developer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
