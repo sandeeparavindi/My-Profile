3905418c654587b1454b64e78d5a268a1c51b02b
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-05-19 19:11:54 +0530

    add a menu button

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -25,17 +25,17 @@ function setActiveStyle(colour) {
 }
 
 //theme light and dark mode
-const dayNight = document.querySelector('.day-night');
-dayNight.addEventListener('click', () => {
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark")
 });
 
 window.addEventListener("load", () => {
-    if (document.body.contains("dark")) {
-        dayNight.querySelector('i').classList.add("fa-sun");
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
     } else {
-        dayNight.querySelector('i').classList.add("fa-moon");
+        dayNight.querySelector("i").classList.add("fa-moon");
     }
 });
