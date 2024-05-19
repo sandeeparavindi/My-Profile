f68962963b159ab895a14fc4840430d6d4a5b437
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-05-19 19:37:46 +0530

    styling menu butto  closing

--- js/Script.js
+++ js/Script.js
@@ -43,3 +43,18 @@ const nav = document.querySelector(".nav"),
        const target = element.getAttribute("href").split("#")[1];
        document.querySelector("#" + target).classList.add("active")
     }
+
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+        aside = document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click", () =>{
+            asideSectionTogglerBtn();
+        })
+
+        function asideSectionTogglerBtn() {
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+            for (let i = 0; i < totalSection; i++) {
+               allSection[i].classList.toggle("open");
+                
+            }
+        }
